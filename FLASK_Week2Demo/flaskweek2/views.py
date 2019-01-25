081f00d3a4edf1a1e26ed037e95b569e5a60f5cf
ominkina <ominkina@gmail.com>
2019-01-25 08:04:00 -0500

    Flask week 2 demo, prior to Fri morning demo

--- FLASK_Week2Demo/flaskweek2/views.py
+++ FLASK_Week2Demo/flaskweek2/views.py
@@ -1,8 +1,9 @@
 import pandas as pd
-df = pd.read_csv("/Users/Olga/Documents/INSIGHT2019/FLASK_Week2Demo/flaskweek2/FlaskCSV.csv")
+df = pd.read_csv("/Users/Olga/Documents/INSIGHT2019/FLASK_Week2Demo/flaskweek2/FlaskCSV_2.csv")
 
 from flask import render_template
 from flaskweek2 import app
+from flask import request
 
 @app.route('/')
 @app.route('/index')
@@ -10,15 +11,19 @@ def index():
 	user = { 'nickname': 'Olga'}
 	return render_template("index.html", title = "Home", user = user)
 
-@app.route('/')
 @app.route('/try1')
 def try1():
 	return str(df.shape)
 
-@app.route('/')
+
 @app.route('/Week2Demo')
 def Week2Demo():
-    Libraries = list(df['LIBNAME_x'])
-    #HelloList = ["hi", "hello"]
-    return render_template('dropdown.html', colours = Libraries)
+    Libraries = list(df['LIBNAME'])
+    return render_template('dropdown.html', libs = Libraries)
+
+@app.route('/submitted', methods=['POST'])
+def submitted():
+    myvariable = request.form.get("Name")
+    strin = "Predicted usage:" + str(df.iloc[df[df.LIBNAME == myvariable].index[0], 2])
+    return strin
 
